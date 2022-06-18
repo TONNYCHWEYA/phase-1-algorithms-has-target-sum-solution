9f3a99b2ac9ff84aa2617ba2958309ade8026fb9
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-18 14:22:45 +0300

    commit tree 04cfed3dde8afbea970bce395ae69f282f5830ac

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
+// function hasTargetSum(array, target) {
+//   function hasTargetSum(array, target) {
+//     for (let i = 0; i < array.length; i++) {
+//       const complement = target - array[i];
+//       for (let j = i + 1; j < array.length; j++) {
+//         if (array[j] === complement) return true;
+//       }
+//     }
+//     return false;
+//   }
+// }
+
+// 
+// function hasTargetSum(array, target) {
+//   // 1 step
+//   const seenNumbers = {};
+//   for (const number of array) {
+//     // n steps
+//     const complement = target - number;
+//     // n steps
+//     if (seenNumbers[complement]) return true;
+//     // n steps
+//     seenNumbers[number] = true;
+//   }
+//   // 1 step
+//   return false;
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Time complexity: O(n)
+Space complexity: O(n)
 */
 
 /* 
   Add your pseudocode here
+
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
+
+
+
+create an object to keep track of all the numbers we've seen
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+    if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+if we reach the end of the array, return false
 */
 
 /*
@@ -29,6 +88,24 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
 }
 
 module.exports = hasTargetSum;
